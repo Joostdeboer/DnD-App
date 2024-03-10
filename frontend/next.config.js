bef92a37a3cd028703280109d12c89a069fdf0e6
Joostdeboer <joostdeboer97@gmail.com>
2024-03-10 16:11:27 +0100

    feat: add vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
-    basePath: '/DnD-App',
-    assetPrefix: '/DnD-App',
+    // basePath: '/DnD-App',
+    // assetPrefix: '/DnD-App',
 };
 
 module.exports = nextConfig;
