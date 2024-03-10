a5b15bebb8a8d1e26cc1ff113182e5432a09132f
Joostdeboer <joostdeboer97@gmail.com>
2024-03-10 19:25:52 +0100

    feat: update next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     // output: 'export',
     // basePath: '/DnD-App',
     // assetPrefix: '/DnD-App',
+    images: {
+        remotePatterns: [{ hostname: 'cdn.sanity.io' }, { hostname: 'source.unsplash.com' }],
+    },
 };
 
 module.exports = nextConfig;
