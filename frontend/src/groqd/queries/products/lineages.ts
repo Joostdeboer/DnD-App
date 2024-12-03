e06dca0689ce289c17bd6679f4f45ca88c25d20a
Joostdeboer <joostdeboer97@gmail.com>
2024-12-03 20:07:33 +0100

    feat: add id, and dates to all objects

--- frontend/src/groqd/queries/products/lineages.ts
+++ frontend/src/groqd/queries/products/lineages.ts
@@ -4,7 +4,10 @@ import { defaultAttributes, information } from '@/src/groqd/helper/subqueries';
 import { getAllByTypeQuery, getTypeQuery } from '@/src/groqd/queries/products/queries';
 
 const Lineage = {
+    _id: q.string(),
+    _createdAt: q.date(),
     _type: q.string(),
+    _updatedAt: q.date(),
     defaultAttributes: q('defaultAttributes').grab$(defaultAttributes).nullable(),
     information: q.object(information).optional(),
     appearance: portableTextQuery('appearance').nullable(),
