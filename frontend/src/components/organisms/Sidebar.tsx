a854803062a72138e6b30209de8d452e80a266af
Joostdeboer <joostdeboer97@gmail.com>
2024-12-10 22:21:26 +0100

    feat: update sidebar styling and dynamic rendering

--- frontend/src/components/organisms/Sidebar.tsx
+++ frontend/src/components/organisms/Sidebar.tsx
@@ -3,6 +3,7 @@ import { documentSettingsQuery } from '@/src/groqd/queries/pages/queries';
 import { DefaultAttributesType, InformationType } from '@/src/groqd/types/subqueries';
 import { Image } from '@/src/components/atoms/generic/Image';
 import { Text } from '@/src/components/atoms/generic/Text';
+import { classNames } from '@/src/utils/functions/classnames';
 
 function MappedSection({
     section,
@@ -18,14 +19,15 @@ function MappedSection({
     if (sectionEntries.length === 0) return null;
 
     return (
-        <div>
-            <Text>{title}</Text>
+        <section className={classNames([sectionEntries.length > 0 ? 'border-b-2 border-brand-primary-900 pb-2' : ''])}>
+            <Text className="text-center pb-2">{title}</Text>
             {sectionEntries.map(([k, v]) => (
-                <Text key={k}>
-                    {titles?.[k] ?? k}: {v}
-                </Text>
+                <section key={k} className="grid grid-cols-2 pl-2">
+                    <Text size="sm">{titles?.[k] ?? k}</Text>
+                    <Text size="sm">{v}</Text>
+                </section>
             ))}
-        </div>
+        </section>
     );
 }
 
@@ -38,66 +40,55 @@ export async function Sidebar({
 }) {
     const sidebarTexts = await runQuery(documentSettingsQuery(), ['sidebar']);
 
+    // get the number of attributes that are added to the product page. If none exist, we need to hide the divider
+    let attributeLength = 0;
+    if (information) {
+        Object.keys(information).forEach((key) => {
+            attributeLength += Object.values(information[key as keyof typeof information] ?? {}).length;
+        });
+    }
+
     return (
         <div className="flex flex-col gap-2">
-            <section className="flex flex-col gap-2 pl-2">
-                {defaultAttributes?.name && <Text>{defaultAttributes?.name}</Text>}
-                {defaultAttributes?.altNames && <Text>{defaultAttributes?.altNames}</Text>}
+            <section
+                className={classNames([
+                    'flex flex-col gap-4 items-center',
+                    attributeLength > 0 ? 'border-b-2 border-brand-primary-900 pb-2' : '',
+                ])}
+            >
+                <section className="flex flex-col items-center">
+                    {defaultAttributes?.name && <Text size="xl">{defaultAttributes?.name}</Text>}
+                    {defaultAttributes?.altNames && (
+                        <Text className="italic" size="sm">
+                            {defaultAttributes?.altNames}
+                        </Text>
+                    )}
+                </section>
                 {defaultAttributes?.image?.asset.url && (
                     <Image
                         src={defaultAttributes?.image?.asset.url}
                         alt={defaultAttributes?.name ?? 'god'}
                         width={1000}
                         height={1000}
-                        className="w-32"
+                        className="!w-32"
+                        priority={true}
+                        fetchPriority="low"
                     />
                 )}
             </section>
-            <MappedSection
-                section={information?.basics}
-                title={sidebarTexts.informationCategories?.basics}
-                titles={sidebarTexts.information?.basics}
-            />
-            <MappedSection
-                section={information?.appearance}
-                title={sidebarTexts.informationCategories?.appearance}
-                titles={sidebarTexts.information?.appearance}
-            />
-            <MappedSection
-                section={information?.commerce}
-                title={sidebarTexts.informationCategories?.commerce}
-                titles={sidebarTexts.information?.commerce}
-            />
-            <MappedSection
-                section={information?.dates}
-                title={sidebarTexts.informationCategories?.dates}
-                titles={sidebarTexts.information?.dates}
-            />
-            <MappedSection
-                section={information?.divinity}
-                title={sidebarTexts.informationCategories?.divinity}
-                titles={sidebarTexts.information?.divinity}
-            />
-            <MappedSection
-                section={information?.geography}
-                title={sidebarTexts.informationCategories?.geography}
-                titles={sidebarTexts.information?.geography}
-            />
-            <MappedSection
-                section={information?.history}
-                title={sidebarTexts.informationCategories?.history}
-                titles={sidebarTexts.information?.history}
-            />
-            <MappedSection
-                section={information?.membership}
-                title={sidebarTexts.informationCategories?.membership}
-                titles={sidebarTexts.information?.membership}
-            />
-            <MappedSection
-                section={information?.relations}
-                title={sidebarTexts.informationCategories?.relations}
-                titles={sidebarTexts.information?.relations}
-            />
+            {/* loop over all keys to dynamically add all mapped sections */}
+            {information &&
+                Object.keys(information).map((k) => {
+                    const key = k as keyof typeof information;
+                    return (
+                        <MappedSection
+                            key={key}
+                            section={information[key]}
+                            title={sidebarTexts.informationCategories?.[key]}
+                            titles={sidebarTexts.information?.[key]}
+                        />
+                    );
+                })}
         </div>
     );
 }
--- frontend/src/components/templates/SidebarPageLayout.tsx
+++ frontend/src/components/templates/SidebarPageLayout.tsx
@@ -16,9 +16,9 @@ export function SidebarPageLayout({
 }) {
     return (
         <PageLayout className={className}>
-            <div className="flex flex-col-reverse md:flex-row justify-between w-full gap-2">
+            <div className="flex flex-col-reverse md:flex-row justify-between items-center md:items-start w-full gap-2">
                 <section>{children}</section>
-                <aside className="bg-brand-neutral-200 dark:bg-brand-neutral-800 rounded-md p-4 border-t-8 border-t-brand-primary-500 h-fit w-4/5">
+                <aside className="bg-brand-neutral-200 dark:bg-brand-neutral-800 rounded-md p-4 border-t-8 border-t-brand-primary-500 h-fit w-full md:w-1/2">
                     <Sidebar defaultAttributes={defaultAttributes} information={information} />
                 </aside>
             </div>
